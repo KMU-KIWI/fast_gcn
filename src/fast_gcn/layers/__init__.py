a6f5c6635eabb2346c3a605c8fec22d1bff9cb41
urw7rs <urw.7rs@icloud.com>
2022-08-22 01:53:59 +0900

    separate model code and layer code

---
+++ src/fast_gcn/layers/__init__.py
@@ -0,0 +1,1 @@
+from .sgn_layers import *
--- src/fast_gcn/layers/sgn_layers.py
+++ src/fast_gcn/layers/sgn_layers.py
@@ -9,37 +9,6 @@ from torch_geometric.nn import Sequential
 from typing import List
 
 
-class SGN(nn.Module):
-    def __init__(self, num_classes, length, num_joints, num_features):
-        super().__init__()
-
-        self.num_classes = num_classes
-        self.length = length
-        self.num_joints = num_joints
-        self.num_features = num_features
-
-        self.dynamics_representation = DynamicsRepresentation(num_features)
-
-        self.joint_level_module = JointLevelModule(num_joints, num_features)
-        self.frame_level_module = FrameLevelModule(length)
-
-        self.fc = nn.Linear(512, num_classes)
-
-    def forward(self, x):
-        # x: N, T, M, V, C
-        N, T, M, V, C = x.size()
-        z = self.dynamics_representation(x)
-        # z: N, T, M, V, C
-        z = self.joint_level_module(z)
-        # z: N, T, M, V, C
-        z = self.frame_level_module(z)
-        # z: N, M, C
-        z = z.mean(dim=1)
-        # z: N, C
-        logits = self.fc(z)
-        return logits
-
-
 class DynamicsRepresentation(nn.Module):
     def __init__(self, num_features):
         super().__init__()
